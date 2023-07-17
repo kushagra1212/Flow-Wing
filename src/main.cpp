b5473825025fe1dec9eeb1aa6c627fda5aeca2ef
kushagra1212 <kushagrarathore002@gmail.com>
2023-07-18 00:02:47 +0530

    -V

--- src/main.cpp
+++ src/main.cpp
@@ -18,61 +18,11 @@ int main() {
   bool seeTree = false;
   std::vector<std::string> text;
   while (true) {
+    std::cout << GREEN << ">>> " << RESET;
+
     std::getline(std::cin, line);
-
     if (line == "") {
-
-      Parser *parser = new Parser(text);
-      CompilationUnitSyntax *compilationUnit =
-          (CompilationUnitSyntax *)parser->parseCompilationUnit();
-      Binder *binder = new Binder();
-
-      compilationUnit->logs.insert(compilationUnit->logs.end(),
-                                   binder->logs.begin(), binder->logs.end());
-
-      if (seeTree)
-        Utils::prettyPrint(compilationUnit->getExpression());
-      if (compilationUnit->logs.size()) {
-        for (int i = 0; i < compilationUnit->logs.size(); i++) {
-          std::cout << RED << compilationUnit->logs[i] << RESET << std::endl;
-        }
-      } else {
-        text = std::vector<std::string>();
-        try {
-
-          Evaluator::logs = std::vector<std::string>();
-          std::any result = Evaluator::evaluate<std::any>(
-              binder->bindExpression(compilationUnit->getExpression()));
-
-          if (Evaluator::logs.size()) {
-            for (int i = 0; i < Evaluator::logs.size(); i++) {
-              std::cout << RED << Evaluator::logs[i] << RESET << std::endl;
-            }
-          } else {
-            if (result.type() == typeid(int)) {
-              int intValue = std::any_cast<int>(result);
-              std::cout << intValue << std::endl;
-            } else if (result.type() == typeid(bool)) {
-              bool boolValue = std::any_cast<bool>(result);
-              std::cout << (boolValue ? "true" : "false") << std::endl;
-            } else if (result.type() == typeid(std::string)) {
-              std::string stringValue = std::any_cast<std::string>(result);
-              std::cout << stringValue << std::endl;
-            } else if (result.type() == typeid(double)) {
-              double doubleValue = std::any_cast<double>(result);
-              std::cout << doubleValue << std::endl;
-            } else if (result.type() == typeid(std::nullptr_t)) {
-
-            } else {
-              throw "Unexpected result type";
-            }
-          }
-
-        } catch (const char *msg) {
-          std::cout << RED << msg << RESET << std::endl;
-          break;
-        }
-      }
+      continue;
     }
     if (line == "`exit") {
       break;
@@ -86,6 +36,57 @@ int main() {
       continue;
     }
     text.push_back(line);
+    Parser *parser = new Parser(text);
+    CompilationUnitSyntax *compilationUnit =
+        (CompilationUnitSyntax *)parser->parseCompilationUnit();
+    Binder *binder = new Binder();
+
+    compilationUnit->logs.insert(compilationUnit->logs.end(),
+                                 binder->logs.begin(), binder->logs.end());
+
+    if (seeTree)
+      Utils::prettyPrint(compilationUnit->getExpression());
+    if (compilationUnit->logs.size()) {
+      for (int i = 0; i < compilationUnit->logs.size(); i++) {
+        std::cout << RED << compilationUnit->logs[i] << RESET << std::endl;
+      }
+    } else {
+      text = std::vector<std::string>();
+      try {
+
+        Evaluator::logs = std::vector<std::string>();
+        std::any result = Evaluator::evaluate<std::any>(
+            binder->bindExpression(compilationUnit->getExpression()));
+
+        if (Evaluator::logs.size()) {
+          for (int i = 0; i < Evaluator::logs.size(); i++) {
+            std::cout << RED << Evaluator::logs[i] << RESET << std::endl;
+          }
+        } else {
+          if (result.type() == typeid(int)) {
+            int intValue = std::any_cast<int>(result);
+            std::cout << intValue << std::endl;
+          } else if (result.type() == typeid(bool)) {
+            bool boolValue = std::any_cast<bool>(result);
+            std::cout << (boolValue ? "true" : "false") << std::endl;
+          } else if (result.type() == typeid(std::string)) {
+            std::string stringValue = std::any_cast<std::string>(result);
+            std::cout << stringValue << std::endl;
+          } else if (result.type() == typeid(double)) {
+            double doubleValue = std::any_cast<double>(result);
+            std::cout << doubleValue << std::endl;
+          } else if (result.type() == typeid(std::nullptr_t)) {
+
+          } else {
+            throw "Unexpected result type";
+          }
+        }
+
+      } catch (const char *msg) {
+        std::cout << RED << msg << RESET << std::endl;
+        break;
+      }
+    }
   }
 
   return 0;
