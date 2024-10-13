794a855bf209d1b7ef8b4453bd2219da007633c3
kushagra1212 <kushagrarathore002@gmail.com>
2024-10-13 15:38:31 +0530

    feat: added icon in lsp

--- src/lsp/server/src/store/completionItems/keywords/conditional.ts
+++ src/lsp/server/src/store/completionItems/keywords/conditional.ts
@@ -68,4 +68,101 @@ The \`else\` keyword is used to declare an else statement.
 		  `,
     },
   },
+  {
+    label: "switch",
+    kind: CompletionItemKind.Keyword,
+    data: "switch",
+    detail: "Switch Statement",
+    documentation: {
+      kind: "markdown",
+      value: `
+**Switch Statement**
+
+The \`switch\` keyword is used to declare a switch statement.
+
+### Example:
+\`\`\`flowwing
+switch (condition) {
+  case 1:
+    /; Code to execute if the condition is 1
+  case 2:
+    /; Code to execute if the condition is 2
+  default:
+    /; Code to execute if the condition is neither 1 nor 2
+}
+\`\`\`
+      `,
+    },
+  },
+  {
+    label: "case",
+    kind: CompletionItemKind.Keyword,
+    data: "case",
+    detail: "Case Statement",
+    documentation: {
+      kind: "markdown",
+      value: `
+**Case Statement**  
+
+The \`case\` keyword is used to declare a case statement.
+  
+### Example:
+\`\`\`flowwing
+switch (condition) {
+  case 1:
+    /; Code to execute if the condition is 1
+  case 2:
+    /; Code to execute if the condition is 2
+  default:
+    /; Code to execute if the condition is neither 1 nor 2
+}
+\`\`\`
+      `,
+    },
+  },
+  {
+    label: "default",
+    kind: CompletionItemKind.Keyword,
+    data: "default",
+    detail: "Default Statement",
+    documentation: {
+      kind: "markdown",
+      value: `
+**Default Statement**
+
+The \`default\` keyword is used to declare a default statement. 
+
+### Example:
+\`\`\`flowwing
+switch (condition) {
+  case 1:
+    /; Code to execute if the condition is 1
+  case 2:
+    /; Code to execute if the condition is 2
+  default:
+    /; Code to execute if the condition is neither 1 nor 2
+}
+\`\`\`
+      `,
+    },
+  },
+  {
+    label: "?",
+    kind: CompletionItemKind.Keyword,
+    data: "questionMark",
+    detail: "Question Mark",
+    documentation: {
+      kind: "markdown",
+      value: `
+**Question Mark**
+
+The \`?\` symbol is used to denote the ternary operator.
+
+### Example:
+\`\`\`flowwing
+var x:str = true ? "Hello" : "Bye"
+\`\`\`
+      `,
+    },
+  },
 ];
