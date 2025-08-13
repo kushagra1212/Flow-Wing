75bf05032af7d6ced623b5a70d0b9fefa6d061b9
kushagra1212 <kushagrarathore002@gmail.com>
2025-08-13 09:38:27 +0530

    style: clean up whitespace in Colors.h for improved code consistency

--- src/utils/Colors.h
+++ src/utils/Colors.h
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * FlowWing Compiler
  * Copyright (C) 2023-2025 Kushagra Rathore
  *
@@ -16,7 +16,6 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-
 
 #pragma once
 
